b7ae1cd720399e3d5e90a3bb7ac357084e848c1f
hychanbb <hoyuichan@gmail.com>
2016-04-26 13:32:48 +0800

    splash page

--- app/src/main/java/com/example/hoyuichan/p2v/Splash.java
+++ app/src/main/java/com/example/hoyuichan/p2v/Splash.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 public class Splash extends Activity {
 
     /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH =3000;
 
     /** Called when the activity is first created. */
     @Override
@@ -25,7 +25,7 @@ public class Splash extends Activity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(Splash.this,Menu.class);
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
                 Splash.this.startActivity(mainIntent);
                 Splash.this.finish();
             }
