15310fad46cfdb44c1630c9828a3548a571d3e9b
Wong Sui On <sowong411@gmail.com>
2016-04-26 16:19:24 +0800

    Welcome Page UI Development

--- app/src/main/java/com/example/hoyuichan/p2v/MainActivity.java
+++ app/src/main/java/com/example/hoyuichan/p2v/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.hoyuichan.p2v;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "fff_Tusj.ttf");
+        TextView myTextView = (TextView) findViewById(R.id.app_name);
+        myTextView.setTypeface(myTypeface);
     }
 
     @Override
