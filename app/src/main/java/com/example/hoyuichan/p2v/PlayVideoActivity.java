ea7e2a1590d066edc92cbce76be2144c731ccc0f
Wong Sui On <sowong411@gmail.com>
2016-04-30 22:58:50 +0800

    Video Generation Implementation

--- app/src/main/java/com/example/hoyuichan/p2v/PlayVideoActivity.java
+++ app/src/main/java/com/example/hoyuichan/p2v/PlayVideoActivity.java
@@ -2,7 +2,6 @@ package com.example.hoyuichan.p2v;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Menu;
@@ -10,16 +9,29 @@ import android.view.MenuItem;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacv.FFmpegFrameRecorder;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+
 import java.io.File;
 import java.util.ArrayList;
 
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+
 public class PlayVideoActivity extends Activity {
+    boolean makevideoThreadDone;
     VideoView videoview;
     String chosenMusicPath;
     String chosenMusicName ;
     int  chosenEffect;
     int chosenTemplate;
-    String[] PhotoPathInStringArray;
+    String[] allPath;
+    File makevideo;
+    ArrayList<opencv_core.Mat> photos = new ArrayList<opencv_core.Mat>();
+    private int transitionFrameDuration;
+    private int mainFrameDuration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +41,59 @@ public class PlayVideoActivity extends Activity {
         chosenMusicName = intent.getStringExtra("chosenMusicName");
         chosenEffect = intent.getIntExtra("chosenEffect", 0);
         chosenTemplate = intent.getIntExtra("chosenTemplate", 0);
-        PhotoPathInStringArray = intent.getStringArrayExtra("PhotoPathInStringArray");
-        File video = new File("/sdcard/DCIM/Camera/combine_old_version.mp4");
-        playVideoOnView(video);
+        allPath = intent.getStringArrayExtra("allPath");
+
+        for (int i=0; i<allPath.length; i++){
+            photos.add(imread(allPath[i]));
+            System.out.println("OK");
+        }
+
+        Thread make_video_thread = new Thread(make_video_worker);
+        make_video_thread.start();
+
+        while(true){
+            if (makevideoThreadDone == false){
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                playVideoOnView(makevideo);
+                break;
+            }
+        }
     }
+
+    private Runnable make_video_worker = new Runnable() {
+        public  void run() {
+            OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
+            makevideo = new File("/sdcard/P2V/makevideo.mp4");
+            if (!makevideo.exists()){
+                makevideo.getParentFile().mkdirs();
+            }
+            FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(makevideo.getAbsolutePath(), 640, 480,1);
+            Frame captured_frame;
+            try {
+                recorder.setFrameRate(20);
+                transitionFrameDuration = 1;
+                mainFrameDuration = 20;
+                recorder.start();
+                for (int i = 0; i < photos.size(); i++) {
+                    captured_frame = converter.convert(photos.get(i));
+                    for (int j = 0; j < mainFrameDuration; j++) {
+                        recorder.record(captured_frame);
+                    }
+                }
+                recorder.stop();
+                recorder.release();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            makevideoThreadDone = true;
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
